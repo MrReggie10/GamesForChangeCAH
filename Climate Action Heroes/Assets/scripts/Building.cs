a185f351ed34a7a9965f4ddc6d27acec05ee7706
AdamLevin7 <63608754+AdamLevin7@users.noreply.github.com>
2022-12-31 15:07:43 -0800

    Grid building progress

---
+++ Climate Action Heroes/Assets/scripts/Building.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+}
