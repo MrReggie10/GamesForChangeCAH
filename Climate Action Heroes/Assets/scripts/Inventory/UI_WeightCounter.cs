8be308e3f2449f3374267e9471cfb73c2b55d4ae
Reggie <bs19293949@outlook.com>
2022-12-30 23:40:56 -0800

    Inventory usability update

---
+++ Climate Action Heroes/Assets/scripts/Inventory/UI_WeightCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class UI_WeightCounter : MonoBehaviour
+{
+    private Transform weightNumber_Container;
+    private Transform weightTop_text;
+    private Transform weightBottom_text;
+
+    void Awake()
+    {
+        weightNumber_Container = transform.Find("WeightNumber_Container");
+        weightTop_text = weightNumber_Container.Find("WeightTop_text");
+        weightBottom_text = weightNumber_Container.Find("WeightBottom_text");
+    }
+
+    public void Refresh(InventorySystem inventory)
+    {
+        TextMeshProUGUI currentWeightText = weightTop_text.GetComponent<TextMeshProUGUI>();
+        currentWeightText.SetText(inventory.getCurrentWeight().ToString());
+
+        TextMeshProUGUI maxWeightText = weightBottom_text.GetComponent<TextMeshProUGUI>();
+        maxWeightText.SetText(inventory.getMaxWeight().ToString());
+    }
+}
